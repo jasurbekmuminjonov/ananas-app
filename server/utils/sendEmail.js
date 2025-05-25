e8242f07ff2acf9e539925c5076ae13d12bdb2bc
Jasur <j31548359@gmail.com>
2025-05-25 18:14:46 +0500

    email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -11,14 +11,14 @@ async function sendEmail(email, otp) {
     });
 
     const mailOptions = {
-      from: "ANANAS APP 🍍 <jasurmominjonov2818@gmail.com>",
+      from: "JASUR21 <jasurmominjonov2818@gmail.com>",
       to: email,
-      subject: "🔐 Verify your account - Ananas.vercel.app",
+      subject: "🔐 Hisobingizni tasdiqlang - jasur21.vercel.app",
       html: `
         <div style="font-family: Arial, sans-serif; padding: 20px; background-color: #f7f7f7;">
-          <h2 style="color: #333;">Ananas Account Verification</h2>
+          <h2 style="color: #333;">Hisobingizni tasdiqlang</h2>
           <p style="font-size: 16px; color: #555;">
-            Please use the following code to verify your account:
+            Sizning tasdiqlash kodingiz:
           </p>
           <div style="margin: 20px auto; width: fit-content; background: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.1); padding: 20px 30px; border-left: 5px solid #00b96b;">
             <h1 style="margin: 0; font-size: 28px; color: #00b96b;">${otp}</h1>
