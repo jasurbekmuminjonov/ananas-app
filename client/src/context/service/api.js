6513b74acc98ca8a3bfccfdf7e2b57c55d4f46f9
Jasur <j31548359@gmail.com>
2025-07-01 09:41:51 +0500

    url fix

--- client/src/context/service/api.js
+++ client/src/context/service/api.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import Cookies from "js-cookie";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080/",
-  // baseUrl: "https://ananas-server.vercel.app/",
+  // baseUrl: "http://localhost:8080/",
+  baseUrl: "https://ananas-server.vercel.app/",
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("token");
     if (token) {
